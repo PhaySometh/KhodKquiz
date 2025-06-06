1414824707cc0b703f2c2cd36b557d6267bfc06f
Um Lyrithyreach <umlyrithyreach@gmail.com>
2025-06-07 01:54:04 +0700

    Revert "Design home"

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
-import image from '../assets/image/heroBackground.png';
 
 export default function Home() {
     return (
         <>
-            <NavBar />
-            <div className="relative mt-20 ">
-                <img src={image} alt='hero background' className='w-full h-auto'/>
-                <h1 className='flex justify-center top-1/2 absolute text-blue-950 text-5xl font-bold w-full item-center'>
-                    Welcome to KhodKquiz
-                </h1>
+            <div className="w-full bg-white">
+              <NavBar />
             </div>
+            <div className="text-2xl text-white-500 text-center mt-5">សូមស្វាគមន៍មកកាន់កូដខ្វីស</div>
         </>
     );
 }
--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -3,17 +3,13 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/home';
 import Leaderboard from '../pages/Leaderboard';
 import Login from '../pages/Login';
-import Quiz from '../pages/Quiz';
-import SignUp from '../pages/SignUp';
 
 function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/quiz" element={<Quiz />} />
-            <Route path="/leaderboard" element={<Leaderboard/>} />
-            <Route path="/signup" element={<SignUp />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/leaderboard" element={<Leaderboard/>} />
         </Routes>
     );
 }
