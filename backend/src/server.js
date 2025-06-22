e7a3810ad7f4969e4406c4755a1e02ae7d432ecd
SeaHuyty <seahuyty4444@gmail.com>
2025-06-22 21:53:15 +0700

    Update: folder structure and implement code readability

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const leaderboardRoutes = require('./routes/leaderboard.routes');
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import leaderboardRoutes from './routes/leaderboard.routes.js';
+
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(helmet());
+app.use(morgan('dev'));
 
 // Routes
 app.use('/api/leaderboard', leaderboardRoutes);
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}/`);
 });
